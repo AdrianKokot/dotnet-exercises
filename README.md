b95095d301ce6679d0c6a7ff3e025ef5f1d8f7de
Adrian Kokot <splateusz@gmail.com>
2022-07-07 12:29:32 +0200

    Algorithm 3

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 1. [Algorytm 1](algorytmy/1)
 2. [Algorytm 2](algorytmy/2)
+3. [Algorytm 3](algorytmy/3)
---
+++ algorytmy/3/Program.cs
@@ -0,0 +1,59 @@
+﻿var rotatedNumbers = new Dictionary<char, char> { { '0', '0' }, { '1', '1' }, { '6', '9' }, { '8', '8' }, { '9', '6' } };
+
+bool isPrime(long n)
+{
+  for (int i = 2; i * i <= n; i++)
+  {
+    if (n % i == 0)
+    {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+bool isStrobogrammatic(string str)
+{
+  for (var i = 0; i < str.Length / 2; i++)
+  {
+    if (!rotatedNumbers.ContainsKey(str[i]) || rotatedNumbers[str[i]] != str[str.Length - i - 1])
+    {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+if (args.Length <= 0)
+{
+  Console.WriteLine("Missing argument.");
+  return;
+}
+
+var input = args[0];
+
+if(!input.All(x => Char.IsNumber(x))) {
+  Console.WriteLine("Input must be a positive number");
+  return;
+}
+
+string resultMessage = "n't strobogrammatic";
+
+if (isStrobogrammatic(input))
+{
+  resultMessage = " strobogrammatic";
+
+  try
+  {
+    resultMessage += isPrime(long.Parse(input)) ? " and prime" : "";
+  }
+  catch (Exception)
+  {
+    resultMessage += " and is too big to check whether is prime";
+  }
+}
+
+
+Console.WriteLine("The given number is" + resultMessage);
