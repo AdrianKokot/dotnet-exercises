6c3d0dcdfa5175645255768b55391f8ac5f475ab
Adrian Kokot <splateusz@gmail.com>
2022-08-02 21:58:54 +0200

    Algorithm 8

--- algorytmy/8/Program.cs
+++ algorytmy/8/Program.cs
@@ -8,24 +8,10 @@ if (args.Length <= 0)
 
 var str = args[0];
 
-var regexes = new (string pattern, bool expectedResult)[] {
-    (@"[^a-zA-Z0-9?!;,\. ]", false),
-    (@" [,;]", false),
-    (@"[,;]([^ ]|$)", false),
-    (@"[^ ][?!\.]$", true),
-    (@"^[A-Z][a-z ]", true),
-    (@"  +", false),
-    (@"[a-zA-Z][0-9]", false),
-    (@"[0-9][a-zA-Z]", false)
-  };
-
-if (regexes.Any(reg => Regex.IsMatch(str, reg.pattern) != reg.expectedResult))
-{
-  Console.ForegroundColor = ConsoleColor.Red;
-  Console.WriteLine("The given string is not a valid sentence.");
-}
-else
-{
+if(Regex.IsMatch(str, "^[A-Ż][a-ż]*( ([A-Źa-ż]+|[0-9]+)[,;]?)*[?!.]$")) {
   Console.ForegroundColor = ConsoleColor.Green;
   Console.WriteLine("The given string is a valid sentence.");
+} else {
+  Console.ForegroundColor = ConsoleColor.Red;
+  Console.WriteLine("The given string is not a valid sentence.");
 }
